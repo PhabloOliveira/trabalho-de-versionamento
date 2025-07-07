4ab997113d83299f5436aad3ebef9979a7b1ac18
Phablo Ribeiro <PhabloRibeiro@gmail.com>
2025-07-07 12:29:46 -0300

    trabalho-de-versionamento - criação menu navegacao

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,16 +1,18 @@
+import { NavigationMenu } from "@/components/ui/navigation-menu";
+
+
+
 export default function Home() {
   return (
     <>
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <h1>
+      <main className="flex h-screen flex-col items-center justify-center p-24">
+         <NavigationMenu />
+
+        <h1 className="text-2xl font-bold text-center mb-8 flex flex-col h-screen">
           Seja bem-vindo ao Trabalho de Versionamento de Software!
-          <br />
         </h1>
-        <p>
-          Todas as informações necessárias realização do trabalho encontra-se no
-          arquivo <span className="">README.md</span>
-        </p>
       </main>
     </>
   );
 }
+
